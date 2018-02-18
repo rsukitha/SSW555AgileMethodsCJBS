bf4a2f0ff1877940604a1099ad7a3b0ed1a70187
Christopher Corrado <ccorrad@its.jnj.com>
2018-02-18 11:46:37 -0500

    US25 US21 #time 1h #comment Updated Unit Tests and Error Output.

--- UniqueNameBirthdateTests.py
+++ UniqueNameBirthdateTests.py
@@ -10,7 +10,7 @@ class GenderForRoleTests(unittest.TestCase):
 
     def test_hs(self):
         """
-        Validate that indi "hs" is valid
+        Validate that if a family contains an invalid id, regardless of where you are in the script, false is returned
         """
         gedcom_data = parse_gedcom_file("./sampledata/us25testdata.ged")
         for family in gedcom_data[0]:
@@ -19,11 +19,11 @@ class GenderForRoleTests(unittest.TestCase):
                 if id in gedcom_data[1]:
                     gedcom_data[1].get(id)
                     if id == "hs":
-                        self.assertTrue(gedcom_data[0].get(family).validate_children(gedcom_data[1]))
+                        self.assertFalse(gedcom_data[0].get(family).validate_children(gedcom_data[1]))
 
     def test_hs2(self):
         """
-        Validate that indi "hs2" is invalid
+        Validate that if a family contains an invalid id, regardless of where you are in the script, false is returned
         """
         gedcom_data = parse_gedcom_file("./sampledata/us25testdata.ged")
         for family in gedcom_data[0]:
@@ -36,7 +36,7 @@ class GenderForRoleTests(unittest.TestCase):
 
     def test_hs3(self):
         """
-        Validate that indi "hs3" is invalid
+        Validate that if a family contains an invalid id, regardless of where you are in the script, false is returned
         """
         gedcom_data = parse_gedcom_file("./sampledata/us25testdata.ged")
         for family in gedcom_data[0]:
@@ -45,6 +45,19 @@ class GenderForRoleTests(unittest.TestCase):
                 if id in gedcom_data[1]:
                     gedcom_data[1].get(id)
                     if id == "hs3":
+                        self.assertFalse(gedcom_data[0].get(family).validate_children(gedcom_data[1]))
+
+    def test_hs4(self):
+        """
+        Validate that if a family contains an invalid id, regardless of where you are in the script, false is returned
+        """
+        gedcom_data = parse_gedcom_file("./sampledata/us25testdata.ged")
+        for family in gedcom_data[0]:
+            children_ids = gedcom_data[0].get(family).children
+            for id in children_ids:
+                if id in gedcom_data[1]:
+                    gedcom_data[1].get(id)
+                    if id == "hs4":
                         self.assertFalse(gedcom_data[0].get(family).validate_children(gedcom_data[1]))
 
 
--- models/Family.py
+++ models/Family.py
@@ -15,18 +15,16 @@ class Family:
 
     def validate_children(self, individuals):
         """
-        U.S. 25 Adds a child to the existing family dictionary if a child with the same name doesn't already
-        exist for this family.
+        U.S. 25 Validates whether a Child with the same name and birthday exists in the family.
         """
-        dup_names = []
-        dup_birthdays = []
-        for child in self.children:
-            if child in individuals:
-                if individuals[child] in dup_names:
-                    print("Duplicate Child's name exists for family: " + individuals[child].name)
-                    dup_names.append(individuals[child].name)
-
-                if individuals[child].birthday in dup_birthdays:
-                    print("Duplicate Child's birthday exists for family: " + individuals[child].birthday)
-                    dup_birthdays.append(individuals[child].birthday)
-        return dup_names == [] and dup_birthdays == []
+        child_name_bday_dict = {}
+        duplicates = []
+        for child_id in self.children:
+            if child_id in individuals:
+                child = individuals[child_id]
+                if child.name in child_name_bday_dict and child_name_bday_dict[child.name] == child.birthday:
+                    duplicates.append(child)
+                    print("ERROR: FAMILY: US25: {}: Child with name: {} and birthday {} already exists".format(
+                        self.id, child.name, child.birthday))
+                child_name_bday_dict[child.name] = child.birthday
+        return duplicates == []
