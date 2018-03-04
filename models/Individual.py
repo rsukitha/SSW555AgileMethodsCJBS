1c3c55f9657280be0d9219399b97ca41dd6b0e1b
55Brian55 <brian.prais@getinge.com>
2018-03-04 10:52:06 -0500

    Updated Individual.PY from Main

--- models/Individual.py
+++ models/Individual.py
@@ -17,7 +17,7 @@ class Individual(Member):
         self.birthday = ""
         self.gender = ""
         self.alive = True
-        self.death = "NA" 
+        self.death = "NA"
 
     def validate_birthday(self):
         if not self.verify_date_150_years(self.birthday):
@@ -25,13 +25,11 @@ class Individual(Member):
                 "ERROR: INDIVIDUAL: US07: {}: Individual Birthday {} over 150 years ago.".format(self.id,
                                                                                                  self.birthday))
 
-
     def validate_death(self):
         if not self.verify_date_150_years(self.birthday):
             print(
                 "ERROR: INDIVIDUAL: US07: {}: Individual Death {} over 150 years ago.".format(self.id,
-                                                                                                 self.birthday))
-
+                                                                                              self.birthday))
 
     def set_age(self):
         self.age = self.calculate_age()
@@ -41,11 +39,7 @@ class Individual(Member):
         Prints whether this user's birthday is upcoming
         :return: True if birthday is upcoming
         """
-        current_year_bday_str = datetime.datetime.strptime(self.birthday, '%d %b %Y').strftime(
-            "%m %d") + " " + str(today.year)
-        current_year_bday = datetime.datetime.strptime(current_year_bday_str, '%m %d %Y')
-        delta_from_birthday = current_year_bday - today
-        if 0 <= delta_from_birthday.days <= 30:
+        if self.return_upcoming_date(self.birthday, today):
             return self
         else:
             return False
@@ -62,14 +56,38 @@ class Individual(Member):
             if death_date <= today:
                 return int(abs(death_date.year)) - int(abs(birthday.year))
         return int(abs(today.year)) - int(abs(birthday.year))
-  
-    def validate_spouse(self, id):
-          
-          if id in self.child:
-                print("ERROR: INDIVIDUAL: US17: {}: Marriage cannot happen with descendants: {}.".format(self.id,
-                                                                                                 id))
-                return False
-          return True
+
+    def validate_spouse(self, child_id):
+
+        if child_id in self.child:
+            print("ERROR: INDIVIDUAL: US17: {}: Marriage cannot happen with descendants: {}.".format(self.id,
+                                                                                                     child_id))
+            return False
+        return True
+
+    def is_dead(self):
+        """
+        US 29: List all deceased individuals in a GEDCOM file
+        :return: True if if dead
+        """
+        if self.death != "NA":
+            print("NOTICE: INDIVIDUAL: US29: {}: Individual Named {} is deceased".format(self.id, self.name))
+            return True
+        return False
+
+    @staticmethod
+    def validate_living_single_over_30(individuals, families):
+        alive_single = set()
+        for indiv in sorted(individuals.items()):
+            for fam in sorted(families.items()):
+                if indiv[1].alive is True and indiv[0] not in fam[1].husband_id or indiv[0] not in fam[1].wife_id:
+                    if Individual.calculate_age(indiv[1]) > 30:
+                        alive_single.add(indiv[0])
+                    else:
+                        print("NOTICE: INDIVIDUAL: US31: {}: is alive and single and less than 30 years old".format(
+                            indiv[0]))
+
+        return alive_single != []
     
     def birth_before_death(self):
         """
@@ -83,4 +101,4 @@ class Individual(Member):
                 print("ERROR: INDIVIDUAL: US03: Individual {}: {}'s Birthday on {} occurs after death date on {}.".format(self.id,
                                                                                        self.name,self.birthday, self.death))
                 return False
-        return True  
+        return True    
